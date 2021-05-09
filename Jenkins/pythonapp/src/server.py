07d29a2b0cd48002d7b7953ae61c4e53e6bedfa3
Maxim Burakov <bmaximxx@gmail.com>
2021-05-09 20:18:18 +0300

    changed text in server.py

--- Jenkins/pythonapp/src/server.py
+++ Jenkins/pythonapp/src/server.py
@@ -3,7 +3,7 @@ server = Flask(__name__)
 
 @server.route("/")
 def hello():
-    return "Hello World! and CI"
+    return "Hello World! and CI+Poll SCM"
 
 if __name__ == "__main__":
    server.run(host='0.0.0.0')
