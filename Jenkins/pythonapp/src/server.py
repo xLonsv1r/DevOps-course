69c1ca0b2eb14079174503de2fa079e12d4ecfc9
Maxim Burakov <bmaximxx@gmail.com>
2021-05-17 21:28:09 +0300

    hello from master

--- Jenkins/pythonapp/src/server.py
+++ Jenkins/pythonapp/src/server.py
@@ -3,7 +3,7 @@ server = Flask(__name__)
 
 @server.route("/")
 def hello():
-    return "Hello World! and CI+Poll SCM"
+    return "Hello World! and CI+Poll SCM and master"
 
 if __name__ == "__main__":
    server.run(host='0.0.0.0', port="5001")
